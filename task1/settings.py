74447bf240cf0e0c050324e153e28eb96c8de7f4
singlasahil221 <singla.sahil221@gmail.com>
2018-06-11 12:23:15 +0530

    home template updated

--- task1/settings.py
+++ task1/settings.py
@@ -88,6 +88,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
 
                 #Social Django
                 'social_django.context_processors.backends',
